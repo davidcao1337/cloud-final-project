6488e301ca0c23daa568b22b285fb2ddfe084505
David Cao <davidcao1337@gmail.com>
2023-04-21 19:12:20 -0400

    Added query options

--- server/controllers/data_analyze.py
+++ server/controllers/data_analyze.py
@@ -1,11 +1,14 @@
 import sys
+import json
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import asc, desc, col
+from pyspark.sql.functions import asc, col
 
 spark = SparkSession.builder.appName("DataAnalyze").getOrCreate()
+spark.sparkContext.setLogLevel("OFF")
 
-# Load household number
-hshd_num = sys.argv[4]
+# Load household number selection & sort selection
+hshd_num_selection = int(sys.argv[4])
+sort_selection = sys.argv[5]
 
 # Load data from csv files
 household_csv_file = sys.argv[1]
@@ -27,13 +30,21 @@ transactions_df = transactions_df.withColumnRenamed("_c0", "BASKET_NUM").withCol
 .withColumnRenamed("_c3", "PRODUCT_NUM").withColumnRenamed("_c4", "SPEND").withColumnRenamed("_c5", "UNITS").withColumnRenamed("_c6", "STORE_R").withColumnRenamed("_c7", "WEEK_NUM") \
 .withColumnRenamed("_c8", "YEAR")
 
-# Sort
-household_df = household_df.orderBy(asc("HSHD_NUM"))
-products_df = products_df.orderBy(asc("PRODUCT_NUM"))
-transactions_df = transactions_df.orderBy(asc("HSHD_NUM"))
-
 # Join Households & Transactions
 househouldTrans_df = household_df.join(transactions_df, on=["HSHD_NUM"], how="inner")
 
 # Join Households-Transactions & Products
 complete_df = househouldTrans_df.join(products_df, on=["PRODUCT_NUM"], how="inner")
+
+# Default sort by HSHD_NUM
+complete_df = complete_df.orderBy(asc("HSHD_NUM"))
+
+# Data Pull for HSHD_SELECTION
+hshd_selection_df = complete_df.filter(col("HSHD_NUM") == hshd_num_selection)
+
+# Sort By Options (HSHD_NUM, BASKET_NUM, DATE, PRODUCT_NUM, DEPARTMENT, COMMODITY)
+hshd_selection_df.orderBy(asc(sort_selection))
+
+# Convert Dataframe to JSON and send to dataController
+json_data = hshd_selection_df.toJSON().map(lambda x: json.loads(x)).collect()
+print(json.dumps(json_data))
